994f5c8d3b92c7a778b8a3f5675f22c863d3b717
ManuchehrTG <toshmatovmanuchehr@gmail.com>
2026-02-16 19:33:36 +0500

    fix proxy DI

--- backend/src/api/v1/imap/dependencies.py
+++ backend/src/api/v1/imap/dependencies.py
@@ -1,13 +1,19 @@
 from fastapi import Depends
 
 from src.application.imap.interfaces import IMAPClient
-from src.infrastructure.imap.gmx_imap_client import GMXIMAPClient
 from src.application.imap.use_cases.cloudflare_get_verify_link import CloudflareGetVerifyLink
+from src.domain.interfaces.proxy_client import ProxyClient
+from src.infrastructure.imap.gmx_imap_client import GMXIMAPClient
+from src.infrastructure.proxy.socks5 import ProxySocks5
 
 def get_imap_client() -> IMAPClient:
 	return GMXIMAPClient()
 
+def get_proxy_client() -> ProxyClient:
+	return ProxySocks5()
+
 def get_cloudflare_get_verify_link(
+	proxy_client: ProxyClient = Depends(get_proxy_client),
 	imap_client: IMAPClient = Depends(get_imap_client)
 ):
-	return CloudflareGetVerifyLink(imap_client)
+	return CloudflareGetVerifyLink(proxy_client, imap_client)
--- backend/src/application/imap/use_cases/cloudflare_get_verify_link.py
+++ backend/src/application/imap/use_cases/cloudflare_get_verify_link.py
@@ -11,7 +11,10 @@ class CloudflareGetVerifyLink:
 
 	@handle_domain_errors
 	async def execute(self, command: CloudflareGetVerifyLinkCommand):
-		await self._proxy_client.connection(command.proxy)
-		ip = await self._proxy_client.get_ip()
+		if command.proxy:
+			self._proxy_client.connection(command.proxy)
+
+		ip = self._proxy_client.get_ip()
+
 		link = await self._imap_client.cloudflare_get_verify_link(email_address=command.email, password=command.password)
 		return CloudflareVerifyLinkDTO(email=command.email, link=link, ip=ip)
